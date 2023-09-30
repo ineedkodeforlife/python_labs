086635d2909591ecd460a74ccc002c3cedbdeab1
ineedkodeforlife <109513419+ineedkodeforlife@users.noreply.github.com>
2023-09-30 11:01:56 +0300

    Update lab_2_5_1.py

--- lab-2/lab_2_5_1.py
+++ lab-2/lab_2_5_1.py
@@ -1,19 +1,10 @@
 import os
 
-left = int(input("Введите левую границу (в Кб): "))
-right = int(input("Введите правую границу (в Кб): "))
 
 example_dir = "./example"
+word, counter = input(), 0
 
-count_matching_files = 0
-
-for filename in os.listdir(example_dir):
-    file_path = os.path.join(example_dir, filename)
-    if os.path.isfile(file_path):
-        file_size_kb = os.path.getsize(file_path) / 1024  # 1 Кб = 1024 байта
-
-        if left <= file_size_kb <= right:
-            count_matching_files += 1
-
-# Вывести результат
-print(f"Количество файлов размером от {left} Кб до {right} Кб в директории 'example': {count_matching_files}")
+for filname in os.listdir(example_dir):
+    if word in filname:
+        counter += 1
+print(counter)
